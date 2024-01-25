61c876967d9db7cdefb075bbd1564407dda9ffa9
Latul Alina <latul4834@gmail.com>
2024-01-25 19:02:05 +0200

    Classwok-if

--- Project.java
+++ Project.java
@@ -1,5 +1,12 @@
 public class Project{
 	public static void main(String[] args) {
-			
+			System.out.println("Hello if");
+
+			double x=20;
+			if(x % 2 ==0 ){
+				System.out.println("par");
+			} else {
+				System.out.println("impar");
+			} 
 		}	
 }
